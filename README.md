5472374b7b18f938c124ab43c3efc8188429ddae
Nicholas P. Meyer <nmeyer5435@gmail.com>
2017-01-17 19:41:04 -0600

    Edited readme, added sample plot outputs, redid some scaling and units

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Thermodynamics Data Generator -- Physics Bridge
 ## Nick Meyer and Aaron Wangberg
+![Sample Output Plot][plot1]
+
+
+[plot1]: https://github.com/nick5435/thermo-bridge/raw/master/plots/TPS.png "Sample Plot"
--- python/data_grabber_2.py
+++ python/data_grabber_2.py
@@ -8,12 +8,13 @@ from mpl_toolkits.mplot3d import Axes3D
 
 FLUID = "Water"
 NUM_POINTS = 250
+COLORMAP = "nipy_spectral"
 
 
 T = np.linspace(CP.PropsSI(FLUID, "TMIN") + 0.1,
                 CP.PropsSI(FLUID, "TMAX") - 0.1, NUM_POINTS)
-P = np.linspace(CP.PropsSI(FLUID, "PMIN") + 1,
-                CP.PropsSI(FLUID, "PMAX") - 1, NUM_POINTS)
+P = np.linspace(CP.PropsSI(FLUID, "PMIN") + 1000,
+                CP.PropsSI(FLUID, "PMAX") - 1000, NUM_POINTS)
 
 
 # S = CP.PropsSI("S", "T", T, "P", P, FLUID)
@@ -22,15 +23,28 @@ data = []
 
 for t in T:
     for p in P:
-        data.append([t, p, CP.PropsSI("S", "T", t, "P", p, FLUID)])
+        data.append([t, p * (10**-5) / 1000,
+                     CP.PropsSI("S", "T", t, "P", p, FLUID)])
 rawdata = np.asarray(data)
-
 newdata = []
 for i, s in enumerate(rawdata[:, 2]):
-    if s > 0:
-        newdata.append([rawdata[i, 0], rawdata[i, 1], np.log10(rawdata[i, 2])])
+    if s > 0 and rawdata[i, 1] >= 10**-2 and rawdata[i, 0] > 0:
+        newdata.append([rawdata[i, 0], rawdata[i, 1], rawdata[i, 2]])
 
-newdata = np.asarray(newdata)
-frame = pd.DataFrame(newdata, columns=['T', 'P', 'S'])
+# newdata = np.asarray(newdata)
+frame = pd.DataFrame(newdata, columns=["T", "P", "S"])
+# print(frame.head(5))
+frame.to_csv("data/TPS.csv")
 
-frame.to_CSV("data/TPS.csv")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(frame["T"], frame["P"], frame["S"], c=frame[
+           "S"], cmap=COLORMAP, edgecolors="none")
+# for T, V, S in zip(frame["T"],frame["P"], frame["S"]):
+#     ax.scatter(T, V, S)
+
+ax.set_xlabel("T")
+ax.set_ylabel("P (kBar)")
+ax.set_zlabel("S")
+ax.set_title("T and P vs S")
+plt.show()
