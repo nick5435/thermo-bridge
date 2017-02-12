78494c6563630155a14b22c1ecc07e91881f987f
Nicholas P. Meyer <nmeyer5435@gmail.com>
2017-02-12 11:31:20 -0600

    Edited Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,7 @@ import sys
 
 import sphinx_bootstrap_theme
 from recommonmark.parser import CommonMarkParser
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../thermoDataGrabber/'))
 sys.path.insert(0, os.path.abspath('../../notebooks/'))
@@ -30,7 +31,19 @@ extensions = [
     'sphinx.ext.githubpages', 'sphinx.ext.napoleon', 'sphinx_autodoc_typehints'
 ]
 
-autodoc_mock_imports = ['pyrsistent','arrow']
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+autodoc_mock_imports = ['pyrsistent', 'arrow']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
