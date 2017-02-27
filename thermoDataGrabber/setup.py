635fafd83565edf966d5d69c68524ddf8c8a77f6
Nicholas P. Meyer <nmeyer5435@gmail.com>
2017-02-26 22:45:30 -0600

    Oops, forgot to change configs

--- thermoDataGrabber/setup.py
+++ thermoDataGrabber/setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='thermoDataGrabber',
-    version='0.4.2',
+    version='0.5.0',
     description='Creation of Thermodynamic Surfaces using CoolProp',
     long_description=readme(),
     url='https://github.com/nick5435/thermo-bridge',
@@ -19,19 +19,11 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        'CoolProp>=6.0.0',
-        'matplotlib>=2.0.0',
-        'numpy>=1.12.0',
-        'pandas>=0.19.2',
-        'sphinx>=1.5.1',
-        'sphinx_bootstrap_theme>=0.4.13',
-        'sphinx-autobuild>=0.6.0',
-        'sphinx-autodoc-typehints>=1.1.0',
-        'recommonmark>=0.4.0',
-        'pypandoc>=1.3.3',
-        'typing>=3.5.3.0',
-        'pyrsistent>=0.12.0',
-        'arrow>=0.10.0',
+        'CoolProp>=6.0.0', 'matplotlib>=2.0.0', 'numpy>=1.12.0',
+        'pandas>=0.19.2', 'sphinx>=1.5.1', 'sphinx_bootstrap_theme>=0.4.13',
+        'sphinx-autobuild>=0.6.0', 'sphinx-autodoc-typehints>=1.1.0',
+        'recommonmark>=0.4.0', 'pypandoc>=1.3.3', 'typing>=3.5.3.0',
+        'pyrsistent>=0.12.0', 'arrow>=0.10.0', 'cytoolz>=0.8.2'
     ],
     python_requires='>=3.6.0',
     include_package_data=True,
